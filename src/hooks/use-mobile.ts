c15481a915ef05298b2b569d650c7cb24218af34
asitis <asitisdev@gmail.com>
2026-04-29 19:42:28 +0900

    fix: use-mobile 훅 수정 (#1)

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -1,19 +1,27 @@
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
+const MOBILE_QUERY = `(max-width: ${MOBILE_BREAKPOINT - 1}px)`
+
+function getSnapshot() {
+  if (typeof window === "undefined" || !window.matchMedia) {
+    return false
+  }
+
+  return window.matchMedia(MOBILE_QUERY).matches
+}
+
+function subscribe(callback: () => void) {
+  if (typeof window === "undefined" || !window.matchMedia) {
+    return () => {}
+  }
+
+  const mql = window.matchMedia(MOBILE_QUERY)
+
+  mql.addEventListener("change", callback)
+  return () => mql.removeEventListener("change", callback)
+}
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
-
-  React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
-
-  return !!isMobile
+  return React.useSyncExternalStore(subscribe, getSnapshot, () => false)
 }
